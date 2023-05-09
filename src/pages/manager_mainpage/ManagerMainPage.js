a87c311669e7ddc3965ec8daaf2aa3bf2494684c
fateme <84022001+fatemebd@users.noreply.github.com>
2023-05-10 02:17:47 +0430

    Adding contexts

--- src/pages/manager_mainpage/ManagerMainPage.js
+++ src/pages/manager_mainpage/ManagerMainPage.js
@@ -5,18 +5,9 @@ import Header from "../../components/adminHeader";
 import Table from "../../components/Table";
 import Footer from "../../components/Footer"
 import SearchBox from "../../components/SearchBox";
-import managerData from "../../MOCK_DATA (1).json"
 
 const arr=['icon1','تاریخ پایان','پرسشنامه','کارمند','نام نظرسنجی','ردیف'];
 const ManagerMainPage = () => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:8000/questionnaire/questionnaires/")
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error(error));
-  }, []);
-
   return (
     <div className="managerMainPage">
       <Header />
