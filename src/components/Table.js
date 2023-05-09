76b1ac0ab4531d914868e61d0edfca705369b38d
fateme <84022001+fatemebd@users.noreply.github.com>
2023-05-10 02:04:58 +0430

    Adding managerMainPage context file

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+<<<<<<< Updated upstream
 import Icon, { Result } from "../components/icon";
 import  "../styles/Table.css"
 const icon1=false;
@@ -10,12 +11,35 @@ function Table({arr}) {
       .then(data => setData(data))
       .catch(error => console.error(error));
   }, []);
+=======
+import Icon, { Result,Edit,Delete } from "../components/icon";
+
+import {useManagerMainContext} from "../contexts/managerMainContext"
+
+import  "../styles/Table.css"
+let iconManager=false;
+let check=false;
+let fillIcon=false;
+let userList=false;
+
+
+function Table(props) {
+  
+   const [data,setData]=useManagerMainContext();
+  setData(props.data);
+  
+  let counter=1;
+>>>>>>> Stashed changes
   return (
     <div >
       <table>
         <thead>
           <tr>
+<<<<<<< Updated upstream
             {arr.map((element) =>{
+=======
+            {props.columns.map((element) =>{
+>>>>>>> Stashed changes
                 switch(element){
                   case "icon1":{
                     return <th> </th>;
@@ -31,6 +55,7 @@ function Table({arr}) {
         </thead>
         
         <tbody>
+<<<<<<< Updated upstream
           {data.map((item,key) => (
             <tr key={key}>
                <td><Result /></td> 
@@ -39,6 +64,27 @@ function Table({arr}) {
               <td>{item.body}</td>
             </tr>
           ))}
+=======
+        {[data].map((item) => (  
+          <tr key={item.id}>
+            {iconManager &&(
+              <td className="iconManager"><Result /></td>
+            )}
+            {userList&&(
+              <td className="usersListIcons">
+                <div className="icon_container">
+                  <Edit />
+                  <Delete />
+                </div>
+              </td>
+            )}
+            {props.values.map((element)=>
+              <td>{item[element]}</td>                
+            )}
+            <td className="rowNum">{counter++}</td>
+          </tr>
+        ))}
+>>>>>>> Stashed changes
         </tbody>
       </table>
     </div>
--- src/pages/manager_mainpage/ManagerMainPage.js
+++ src/pages/manager_mainpage/ManagerMainPage.js
@@ -1,19 +1,37 @@
+<<<<<<< Updated upstream
 import React from "react";
 import "./main.css";
+=======
+import React, { useState, useEffect } from "react";
+import "./main1.css";
+>>>>>>> Stashed changes
 import "./global.css";
 import Header from "../../components/adminHeader";
 import Table from "../../components/Table";
 import Footer from "../../components/Footer"
 import SearchBox from "../../components/SearchBox";
+import managerData from "../../MOCK_DATA (1).json"
 
 const arr=['icon1','تاریخ پایان','پرسشنامه','کارمند','نام نظرسنجی','ردیف'];
 const ManagerMainPage = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8000/questionnaire/questionnaires/")
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <div className="managerMainPage">
       <Header />
       <SearchBox />
       <div className="table-body">
+<<<<<<< Updated upstream
       <Table arr={arr} className="table" />
+=======
+      <Table columns={columns} values={values} data={data}  className="table" />
+>>>>>>> Stashed changes
       </div>
       <Footer />
     </div>
